5e22b5f71de554ac9604cb77c3ec6b7f6d3e54a3
Kai Fuller <kaifuller1995@gmail.com>
2025-12-14 20:40:56 -0700

    troubleshooting 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Libre_Baskerville } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/app/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -9,12 +9,6 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const libreBaskerville = Libre_Baskerville({
-  weight: "400",
-  variable: "--font-libre-baskerville",
   subsets: ["latin"],
 });
 
