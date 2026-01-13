af8933b3beebab9e7eec9f379893b80c044e8568
Kai Fuller <kaifuller1995@gmail.com>
2026-01-12 18:06:25 -0700

    fixed small landing bug

--- app/ui/landing/landing.tsx
+++ app/ui/landing/landing.tsx
@@ -8,15 +8,17 @@ export default function Landing() {
       <ImageCarousel
         images={["/house-1.jpg", "/house-2.jpg", "/house-3.jpg"]}
       />
-      <div className="absolute bottom-30 left-30 m-8 flex flex-col items-center">
-        <h1
-          className="text-4xl animate-slide-right opacity-0 text-[#f6f3ee]"
-          style={{ animationDelay: `${1.5}s` }}
-        >
+      <div
+        className="absolute bottom-30 left-30 m-8 flex flex-col items-center animate-slide-right opacity-0"
+        style={{ animationDelay: `${1.5}s` }}
+      >
+        <h1 className="text-4xl text-[#f6f3ee]">
           Commitment You Feel <br />
           in Every Detail
         </h1>
-        <button className="border text-[#f6f3ee] hover:bg-[#f6f3ee] hover:text-black transition-colors duration-300 p-1 mt-3 rounded cursor-pointer">Find an Estate Manager</button>
+        <button className="border text-[#f6f3ee] hover:bg-[#f6f3ee] hover:text-black transition-colors duration-300 p-1 mt-3 rounded cursor-pointer">
+          Find an Estate Manager
+        </button>
       </div>
     </div>
   );
