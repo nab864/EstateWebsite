03a2ba7850707ec898eaebe742d95b0a58c80250
Kai Fuller <kaifuller1995@gmail.com>
2026-01-24 12:00:29 -0700

    Reduced video size and cached file

--- app/ui/video/video.tsx
+++ app/ui/video/video.tsx
@@ -1,6 +1,15 @@
 export default function Video() {
-
   return (
     <div>
-      <video className="w-full h-full object-cover absolute inset-0" autoPlay playsInline muted loop src="https://res.cloudinary.com/dewpq5xih/video/upload/v1768776498/champagne_fqjzmq.mp4"></video>
-    </div>)}
+      <video
+        className="w-full h-full object-cover absolute inset-0"
+        autoPlay
+        playsInline
+        muted
+        loop
+        src="https://res.cloudinary.com/dewpq5xih/video/upload/f_auto,q_auto,vc_auto,w_1920/v1768776498/champagne_fqjzmq.mp4"
+        
+      ></video>
+    </div>
+  );
+}
